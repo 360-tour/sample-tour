0af9fd6b763a25b9003648f89e7fbf7e142f435a
360-tour <39487457+360-tour@users.noreply.github.com>
2023-05-10 15:41:50 +0100

    upload

---
+++ data.js
@@ -0,0 +1,113 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -1.8069079572773923,
+        "pitch": 0.00895814924563254,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9462407564708588,
+          "pitch": 0.12190013447155401,
+          "rotation": 4.71238898038469,
+          "target": "1-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6400296777237298,
+          "pitch": 0.15300746918904018,
+          "rotation": 1.5707963267948966,
+          "target": "2-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03",
+      "name": "03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 3.128774477128575,
+        "pitch": -0.026949936979328015,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.002633587423927608,
+          "pitch": 0.08082296409180145,
+          "rotation": 0,
+          "target": "0-01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
